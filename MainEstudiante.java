a6cc38e36b7ed871d1c000d69b6b48accc1c0853
Pieralessandro Garcia <103224509+pier1107@users.noreply.github.com>
2022-05-26 16:36:04 -0500

    Update and rename Main (1).java to MainEstudiante.java

--- MainEstudiante.java
+++ MainEstudiante.java
@@ -4,18 +4,18 @@ public class Main
 {
         public static void main(String args[])
         {
-            Curso curso = new Curso("2h 30min",128.4,"colegio Max Plack");
+            Curso curso = new Curso("1h 15min",75.6,"colegio Max Plack");
     
             System.out.println(curso.getHoras());
             System.out.println(curso.getCosto());
             System.out.println(curso.getInstitucion());
            
     
-            curso.setDocente(new Docente("Jose", "Martinez"));
+            curso.setDocente(new Docente("Mario", "Castañeda"));
             System.out.println("nombre del docente: "+curso.getDocente().getNombres());
             System.out.println("apellido del docente; "+curso.getDocente().getApellido());
 
-            curso.setEstudiante(new Estudiante("Alonso", 12345678));
+            curso.setEstudiante(new Estudiante("Paquito", 756984268));
             System.out.println("nombre del alumno: "+curso.getEstudiante().getNombres());
             System.out.println("codigo del alumno: "+curso.getEstudiante().getCodigo());
            
